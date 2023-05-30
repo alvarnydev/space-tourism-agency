9c9a19f6b82da0e75706ddccd6e4abd86ce1fff1
Yannik Sahl <yanniksahl@outlook.de>
2023-05-30 09:33:30 +0200

    Extracting components

---
+++ src/components/HeroButton.tsx
@@ -0,0 +1,9 @@
+const HeroButton = () => (
+  <div className='flex-1 flex justify-end'>
+    <button className='transition-[outline] w-[17rem] h-[17rem] rounded-[50%] bg-white outline-[88px] outline-white/10 hover:outline'>
+      <h4 className='text-black'>Explore</h4>
+    </button>
+  </div>
+);
+
+export default HeroButton;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,8 @@
 import Header from '../components/Header';
+import HeroButton from '../components/HeroButton';
+import HeroText from '../components/HeroText';
 
 function Home() {
-  const HeroText = () => (
-    <div className='flex-1 flex flex-col text-lightBlue tracking-[0.075em]'>
-      <h5>So, you want to travel to</h5>
-      <h1>Space</h1>
-      <p>
-        Let’s face it; if you want to go to space, you might as well genuinely go to outer space and
-        not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly
-        out of this world experience!
-      </p>
-    </div>
-  );
-  const HeroButton = () => (
-    <div className='flex-1 flex justify-end'>
-      <button className='transition-[outline] w-[17rem] h-[17rem] rounded-[50%] bg-white outline-[88px] outline-white/10 hover:outline'>
-        <h4 className='text-black'>Explore</h4>
-      </button>
-    </div>
-  );
-
   return (
     <div className='h-screen bg-cover bg-homeMobile md:bg-homeTablet lg:bg-homeDesktop'>
       <Header />
