2a78d7e47accd9b3c59f76a0c7cb750fb19ac0e5
Yannik Sahl <yanniksahl@outlook.de>
2023-05-30 11:00:18 +0200

    Custom data fetching hook

---
+++ src/hooks/useData.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { LoadingPage } from '../components/loading';
+import { ErrorPage } from '../components/error';
+import { PageDataType } from '../types';
+
+const fetchPageData = async (): Promise<PageDataType> => {
+  const response = await fetch(`/data/data.json`).then((res) => res.json());
+  return response.data;
+};
+
+export function useData(page: 'destinations' | 'crew' | 'technology') {
+  const { isLoading, error, data } = useQuery({
+    queryKey: [page],
+    queryFn: fetchPageData,
+  });
+
+  if (isLoading) return <LoadingPage />;
+  if (error && error instanceof Error) return <ErrorPage errorMessage={error.message} />;
+  if (data === undefined) return <ErrorPage errorMessage='No data found' />;
+
+  return data[page];
+}
---
+++ src/types.ts
@@ -0,0 +1,41 @@
+export type DestinationPageType = [
+  {
+    name: string;
+    images: {
+      png: string;
+      webp: string;
+    };
+    description: string;
+    distance: string;
+    travel: string;
+  }
+];
+
+export type CrewPageType = [
+  {
+    name: string;
+    images: {
+      png: string;
+      webp: string;
+    };
+    role: string;
+    bio: string;
+  }
+];
+
+export type TechnologyPageType = [
+  {
+    name: string;
+    images: {
+      portrait: string;
+      landscape: string;
+    };
+    description: string;
+  }
+];
+
+export type PageDataType = {
+  destinations: DestinationPageType;
+  crew: CrewPageType;
+  technology: TechnologyPageType;
+};
