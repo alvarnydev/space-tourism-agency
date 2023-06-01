3c8dc45e0fd1f33b49ae781513644beb79fccad2
Yannik Sahl <yanniksahl@outlook.de>
2023-06-01 16:09:18 +0200

    Hero button link fix

--- src/components/HeroButton.tsx
+++ src/components/HeroButton.tsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 const HeroButton = () => (
   <div className='flex-1 flex justify-end'>
-    <button className='transition-[outline] w-[17rem] h-[17rem] rounded-[50%] bg-white outline-[88px] outline-white/10 hover:outline'>
-      <Link to={`/destination`}>
+    <Link to={`/destination`}>
+      <button className='transition-[outline] md:w-48 md:h-48 lg:w-[17rem] lg:h-[17rem] rounded-[50%] bg-white outline-[88px] outline-white/10 hover:outline'>
         <h4 className='text-black'>Explore</h4>
-      </Link>
-    </button>
+      </button>
+    </Link>
   </div>
 );
 
