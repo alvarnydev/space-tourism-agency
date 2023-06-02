56e74877cc2b0762a8cf84e664bc2185250e96e2
Yannik Sahl <yanniksahl@outlook.de>
2023-06-02 16:56:57 +0200

    Static assets handling

--- src/utils/preload.ts
+++ src/utils/preload.ts
@@ -1,16 +1,21 @@
 export function preloadAssets(page: string) {
   import.meta.glob('../assets/shared/*');
+  console.log('shared assets preloaded');
 
   if (page === 'home') {
     import.meta.glob('../assets/home/*');
+    console.log('home assets preloaded');
   }
   if (page === 'crew') {
     import.meta.glob('../assets/crew/*');
+    console.log('crew assets preloaded');
   }
   if (page === 'destination') {
     import.meta.glob('../assets/destination/*');
+    console.log('destination assets preloaded');
   }
   if (page === 'technology') {
     import.meta.glob('../assets/technology/*');
+    console.log('technology assets preloaded');
   }
 }
