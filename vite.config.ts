f5dc8c2b352fe2ae24952d7952c3d5960603797b
Yannik Sahl <yanniksahl@outlook.de>
2023-06-10 12:00:21 +0200

    Webp added to assetsInclude

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg'],
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.webp'],
   plugins: [react()],
   server: {
     host: true,
